45d050f5c5e8ed279ca3e78e3bfde01697281cae
Amitsingh-doctm <81507308+Amitsingh-doctm@users.noreply.github.com>
2021-03-28 16:21:58 +0530

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,19 @@
+const fs = require('fs'); 
+// Calling the readFileSync() method 
+// to read 'input.txt' file 
+
+//let input = process.argv.slice(2);
+//console.log(input[0]);
+
+//const data = fs.readFileSync('dir', 
+         //   {encoding:'utf8', flag:'r'}); 
+  
+// Display the file dataconsole.log(a);
+
+       let text = `Amit singh is good men
+
+
+       fgghgjgvnc`
+       text =text.replace(/\n\s*\n/g, '\n');
+
+      console.log(text)
